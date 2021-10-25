e4306c4372dcf30b70d7510e436fb54bd48c0bf1
mvciek <mpeciakk@gmail.com>
2021-10-25 21:20:46 +0200

    authentication using mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { MongooseModule } from '@nestjs/mongoose'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
 import { UsersModule } from './users/users.module'
 
 @Module({
-    imports: [AuthModule, UsersModule],
+    imports: [
+        AuthModule,
+        UsersModule,
+        ConfigModule.forRoot(),
+        MongooseModule.forRoot(
+            `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_URL}/${process.env.DB_NAME}`,
+        ),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,23 +1,15 @@
 import { Injectable } from '@nestjs/common'
-
-export type User = any
+import { InjectModel } from '@nestjs/mongoose'
+import { Model } from 'mongoose'
+import { User, UserDocument } from './user.schema'
 
 @Injectable()
 export class UsersService {
-    private readonly users = [
-        {
-            userId: 1,
-            username: 'john',
-            password: 'changeme',
-        },
-        {
-            userId: 2,
-            username: 'maria',
-            password: 'guess',
-        },
-    ]
+    constructor(
+        @InjectModel(User.name) private userModel: Model<UserDocument>,
+    ) {}
 
-    async findOne(username: string): Promise<User | undefined> {
-        return this.users.find((user) => user.username === username)
+    async findOne(username: string): Promise<User> {
+        return this.userModel.findOne({ username }).exec()
     }
 }
