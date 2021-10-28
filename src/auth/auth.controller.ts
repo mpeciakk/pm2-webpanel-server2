0ae482d132b42b889c482e18a72245031e1e5e91
mvciek <mpeciakk@gmail.com>
2021-10-28 22:57:53 +0200

    class validator for request body & login dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,16 @@
-import { Controller, Post, Request } from '@nestjs/common'
+import { Body, Controller, Post } from '@nestjs/common'
 import { AuthService } from './auth.service'
+import { LoginUserDto } from './login-user.dto'
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) {}
 
     @Post('login')
-    async login(@Request() req) {
+    async login(@Body() loginDto: LoginUserDto) {
         const user = await this.authService.validateUser(
-            req.query.username,
-            req.query.password,
+            loginDto.username,
+            loginDto.password,
         )
 
         return this.authService.login(user)
